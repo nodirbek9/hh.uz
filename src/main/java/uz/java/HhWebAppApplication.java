401418004c03658b64a507f734465484c809cb6b
bekhruz_web <your-email@example.com>
2026-04-12 16:18:33 +0500

    Initial commit

---
+++ src/main/java/uz/java/HhWebAppApplication.java
@@ -0,0 +1,13 @@
+package uz.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/java/HhWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
