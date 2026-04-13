1b9724aa7308899c6e52f64eb09eb2272a145a7c
user <user@MacBook-Air-user.local>
2026-04-13 18:26:38 +0300

    first commit Nodirbek

---
+++ src/main/java/uz/java/entity/UserProfile.java
@@ -0,0 +1,13 @@
+package uz.java.entity;
+
+import java.util.Date;
+
+public class UserProfile {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String avatar;
+    private Date birthDate;
+    private String city;
+    private String country;
+}
